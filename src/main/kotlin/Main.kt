c19feddede5d86310d66699eed295da090b85079
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-16 00:21:28 +0300

    make the code look better

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,39 @@
 fun main() {
     val name = "Madrigal"
-    val healthPoints = 66
+    var healthPoints = 4
     val isBlessed = true
     val isImmortal = false
     val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20 ).toInt()
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
 
-    val aura1 = when (karma) {
-        in 0..5 -> "red"
-        in 6..10 -> "purple"
-        in 11..15 -> "orange"
-        in 16..20 -> "green"
-        else -> "none"
-
+    val Auracolor = when (karma){
+        in 0..5 -> "Red"
+        in 6..10 -> "Orange"
+        in 11..15 -> "Purple"
+        in 16..20 -> "Green"
+        else -> "NONE"
     }
+    println(karma)
+    val auraVisible = isBlessed && healthPoints >50 || isImmortal
 
-    val auraColor = if(auraVisible)(aura1) else "none"
-    println("(Your Karma: $karma ) (and your aura color is : $aura1) " +
-            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+
+    val auraColor = if (auraVisible)(Auracolor)else "None"
+    //println(auraColor)
+
+    println("(Aura:$auraColor)" + "(Blessed: ${if (isBlessed) "Yes" else "No"})")
 
 
     val healthStatus = when (healthPoints) {
-        100 -> "is in excellent condition!"
+        100 -> "in in excelent condtion!"
         in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) {
             "has some minor wounds but is healing quite quickly!"
-        } else {
+        }
+        else {
             "has some minor wounds."
         }
         in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
+        else ->"is in awful condition!"
     }
-    println("$name $healthStatus $auraColor ")
+    println(karma)
+    println("$name $healthStatus")
 }
-
